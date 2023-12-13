61df90e2683efbfad603b282ce12f60579078f2d
krabuk97 <125387726+krabuk97@users.noreply.github.com>
2023-12-13 14:24:55 +0000

    13/12/2023

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def home_page():
+def hello_world():
     return render_template("home.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8080)
